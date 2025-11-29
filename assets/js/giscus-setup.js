246e5c513090ef879d550d82dc3ab9b35dc09b1e
chrystalchern <52893467+chrystalchern@users.noreply.github.com>
2025-11-29 00:20:43 +0000

    Deploying to gh-pages from @ chrystalchern/chrystalchern.github.io@a2ea60aeae0aad03649696721de899d837e18017 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "chrystalchern/chrystalchern",
+    "data-repo": "chrystalchern/chrystalchern.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
