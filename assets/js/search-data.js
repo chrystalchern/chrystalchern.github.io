23e1736ee0b4c8ac88548062a0702d797e97abd0
chrystalchern <52893467+chrystalchern@users.noreply.github.com>
2026-06-15 07:11:43 +0000

    Deploying to gh-pages from @ chrystalchern/chrystalchern.github.io@aa09a6791172aaa7cccc9a65c07df6ce92b45dfd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -444,7 +444,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=6wZSFMAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=PGFeUWYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
