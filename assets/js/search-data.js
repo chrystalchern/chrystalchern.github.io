650a2f47475e10ba6f2c40a99a4a696012294cf6
chrystalchern <52893467+chrystalchern@users.noreply.github.com>
2026-06-19 21:30:45 +0000

    Deploying to gh-pages from @ chrystalchern/chrystalchern.github.io@e6fbc00827ce8d439271bbd66259217b6f3a6d21 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "Chrystal Chern&#39;s CV",
           section: "Navigation",
           handler: () => {
